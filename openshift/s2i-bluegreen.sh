4fb3ecb2ef92d9c28189e6e5d4fa06cc927d3b4d
Carlos Simon <casimon@redhat.com>
2021-09-03 01:18:16 -0500

    version

--- openshift/s2i-bluegreen.sh
+++ openshift/s2i-bluegreen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 oc import-image ubi8/openjdk-8 --from=registry.access.redhat.com/ubi8/openjdk-8 --confirm
 for color in $(echo "blue green"); do
-  oc new-app openjdk-8-ubi8~https://github.com/casimon-rh/spring-docker-hello#$color --name java-$color -p JAVA_APP_JAR=demo.jar
+  oc new-app openjdk-8~https://github.com/casimon-rh/spring-docker-hello#$color --name java-$color -p JAVA_APP_JAR=demo.jar
   oc rollout pause deployment/java-$color
   oc set probe deployment/java-$color --remove --readiness --liveness
-  oc set probe deployment/java-$color --liveness --get-url=http://:8080/actuator/health --initial-delay-seconds=240
-  oc set probe deployment/java-$color --readiness --get-url=http://:8080/actuator/health --initial-delay-seconds=240
+  oc set probe deployment/java-$color --liveness --get-url=http://:8080/actuator/health --initial-delay-seconds=40
+  oc set probe deployment/java-$color --readiness --get-url=http://:8080/actuator/health --initial-delay-seconds=40
   oc create cm s2i-appconfig-$color --from-literal=MY_APPLICATION_NAME=java-$color
   oc set env deployment/java-$color --from=configmap/s2i-appconfig-$color
   oc rollout resume deployment/java-$color
