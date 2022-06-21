601f9cb5bf05adb2703302f288bc404981c0cc51
Carlos Simon <casimon@redhat.com>
2022-06-21 00:39:18 -0500

    podman

--- docker-scripts/java.sh
+++ docker-scripts/java.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mvn clean package -f ../pom.xml -P jar
-docker build --compress -f ./docker/java.dockerfile -t demo-java ..
-docker run -e MY_APPLICATION_NAME=java --rm -p 8080:8080 -it demo-java 
+podman build --compress -f ./docker/java.dockerfile -t demo-java ..
+podman run -e MY_APPLICATION_NAME=java --rm -p 8080:8080 -it demo-java 
