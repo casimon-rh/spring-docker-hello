ae9eb8a47192abad8757629ccca75b538f65424a
Carlos Simon <casimon@redhat.com>
2022-06-21 01:10:28 -0500

    shorter wait

--- openshift/s2i.sh
+++ openshift/s2i.sh
@@ -3,8 +3,8 @@ oc import-image jboss-eap-7/eap73-openjdk11-openshift-rhel8:latest --from=regist
 oc new-app eap73-openjdk11-openshift-rhel8~https://github.com/casimon-rh/spring-docker-hello --name jboss-s2i
 
 oc set probe deployment/jboss-s2i --remove --readiness --liveness
-oc set probe deployment/jboss-s2i --liveness --get-url=http://:8080/demo/actuator/health --initial-delay-seconds=240
-oc set probe deployment/jboss-s2i --readiness --get-url=http://:8080/demo/actuator/health --initial-delay-seconds=240
+oc set probe deployment/jboss-s2i --liveness --get-url=http://:8080/demo/actuator/health --initial-delay-seconds=120
+oc set probe deployment/jboss-s2i --readiness --get-url=http://:8080/demo/actuator/health --initial-delay-seconds=120
 
 oc create route edge --service=jboss-s2i
 
